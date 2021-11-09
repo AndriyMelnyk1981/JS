7f142061b1597276a3f512a946d0b5674b55f326
Andriy Melnyk <andriy.l.melnyk@globallogic.com>
2021-11-09 13:51:59 +0200

    lowercase

--- Fn/Fn.js
+++ Fn/Fn.js
@@ -1,8 +1,19 @@
-function sayHelloWorld() {
-  return "Hello, World!";
+// function sayHelloWorld() {
+//   return "Hello, World!";
+// };
+// var str = sayHelloWorld();
+// console.log(str);
+const Marvel = {
+  yearCreation: 1939,
+  mainCharacter: 'Spider Man',
+ mainTeam: 'Avengers',
 };
-var str = sayHelloWorld();
-console.log(str);
-
-
- 
+function extendObject(obj, field, value) {
+  const newObj = Object.assign({}, obj);
+  newObj[field] = value;
+  
+  return newObj;
+}
+const NewMarrvel = extendObject(Marvel, 'mainVillain', 'Venom');
+ console.log('Marvel object: ', Marvel);
+ console.log('NewMarvel object: ', NewMarvel);
