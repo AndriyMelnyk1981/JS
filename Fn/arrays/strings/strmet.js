7a759a9620fac5ec6081428f49fbbe73f90397ae
Andriy Melnyk <andriy.l.melnyk@globallogic.com>
2021-11-18 17:27:49 +0200

    strings

--- Fn/arrays/strings/strmet.js
+++ Fn/arrays/strings/strmet.js
@@ -1,8 +1,88 @@
-const director = {
-    firstName: 'Steven',
-    lastName: 'Spielberg',
-    movies: 57,
-};
-const info = 'As a Director ' + director.firstName + ' ' + director.lastName + ' created '
- + director.movies + ' movies. ';
-console.log(info)
+// const director = {
+//     firstName: 'Steven',
+//     lastName: 'Spielberg',
+//     movies: 57,
+// };
+// const info = 'As a Director ' + director.firstName + ' ' + director.lastName + ' created '
+//  + director.movies + ' movies. ';
+// console.log(info);
+
+// const myArr = ['Sunday', 'Monday,', 'Tuesday'];
+// const myNewArr = 
+
+// const lines = 0.75;
+// const language = 'JavaScript';
+// const result = `${lines > 0 ? lines: 'Zero'} lines of ${language} code`;
+// console.log(result);
+
+// const sentence = 'The quick brown fox jumps over lazy dog.';
+// const index = 6;
+// const char = sentence.charAt(index);
+// console.log('The character at index 6 is', char)
+
+// const sentence = 'The quick brown cat jumps over lazy dog.';
+// const word = 'fox';
+// const hasWord = sentence.includes(word);
+// console.log(`The word '${word}' ${hasWord ? 'is' : 'is not'} in the sentence`);
+// const hasCatWord = sentence.includes('cat');
+
+	
+// const sentence = 'The quick brown fox jumps over the lazy dog.';
+ 
+// const word = 'fox';
+// const hasWord = sentence.includes(word); // true
+ 
+// console.log(`The word "${word}"
+// 	${hasWord ? 'is' : 'is not'} in the sentence`);
+// // output: "The word "fox" is in the sentence"
+ 
+// const hasCatWord = sentence.includes('cat'); // false
+
+	
+// const beasts = 'Beasts list: ant, python, camel, duck, bison';
+ 
+// console.log(beasts.indexOf('bison'));
+// // output: 39 
+ 
+// console.log(beasts.indexOf('tiger'));
+// // output: -1
+
+// const array = ["one", "two", "three"];
+// const str = array.toString();
+// const convert = str.toUpperCase();
+// str = convert.split();
+// console.log(convert);
+
+// const paragraph = "The quick brown fox jumps over the lazy dog. It barked.";
+// const regex = /[A-Z]/g;
+// const found = paragraph.match(regex);
+// console.log(found);
+
+// const p = "The quick brown fox jumps over the lazy dog."
+// const p1 = "The quick brown fox jumps over the lazy dog."
+// console.log(p.replace('dog', 'monkey'));
+
+// const dogRegex = /Dog/i;
+// console.log(p1.replace(dogRegex, 'ferret'));
+
+// const greeting = '   Hello, world!      ';
+// console.log(greeting.trim());
+
+// let array = ['Sunday', 'Monday,', 'Tuesday']
+// let newArr = array.map(function (current) {
+//     console.log(current.toUpperCase());
+//   });
+
+
+let str = "Argentina manit negra";
+function palindrome(str) {
+    var removeChar = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+    var checkPalindrome = removeChar.split('').reverse().join('');
+     return (removeChar === checkPalindrome);
+  }
+ 
+
+// let newParagraph = paragraph.map(function (current) {
+//     return current.toLowerCase();
+//   });
+//   console.log(current)
---
+++ Fn/objects/objects.js
@@ -0,0 +1,5 @@
+let obj = {};
+obj['name'] = "Igor";
+obj['surname'] = "Pistachio"
+obj['age']= 42;
+console.log(obj['name'], obj['surname'], obj['age']= 42)
