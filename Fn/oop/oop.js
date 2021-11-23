49fe3a2490962f2751043f6f2feea3f98cf655c7
Andriy Melnyk <andriy.l.melnyk@globallogic.com>
2021-11-23 10:02:55 +0200

    oop

--- Fn/oop/oop.js
+++ Fn/oop/oop.js
@@ -43,21 +43,25 @@ console.log("\nТак, я адмін, не завидуйте.")
 else {console.log("\nНі, я не адмін, шукайте когось іншого.")}
 }
 }
+
+
+// console.log(u1.myInfo());
+class Moderator extends User {
+    constructor(name, age, admin=false, moder = true) {
+        super(name, age, admin= false)
+               this.moder = moder;
+    } 
+    addPost() {
+        console.log("Я дуже сильний, пусти сорочку")
+    }
+        myInfo() {
+            super.myInfo();
+            console.log("Я модератор")
+        
+}
+}
+let m1 = new Moderator ("Pavlo", 34, true)
 let u1 = new User("Jack", 25);
 let u2 = new User("John", 23, true);
 let u3 = new User("Mike", 24);
-// console.log(u1.myInfo());
-class Moderator extends User {
-    addPost() {
-        console.log("Я дуже сильний, пусти сорочку")}
-        myInfo() {
-            console.log("Мене звати " +this.name+ ", мені " + this.age+ " роки.")
-        if(this.admin == true)
-        {
-        console.log("\n Я модератор.")
-        }
-}
-}
-
-let m1 = new Moderator ("Pavlo", 34, true)
-console.log(m1.myInfo());
+console.log(m1.addPost());
